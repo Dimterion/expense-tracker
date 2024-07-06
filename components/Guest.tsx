baf1a4f24dbecdc47ceeb6aea84bef1f19e97aac
Dmitrii <dimterion59@gmail.com>
2024-07-06 12:28:29 +0200

    add cover img to Guest component

--- components/Guest.tsx
+++ components/Guest.tsx
@@ -1,9 +1,18 @@
+import Image from "next/image";
 import { SignInButton } from "@clerk/nextjs";
+import coverImage from "../assets/images/cover_img.jpg";
 
 const Guest = () => {
   return (
     <section className="guest-section">
-      <h2>Welcome!</h2>
+      <h1 className="guest-h1">Expense Tracker App</h1>
+      <h2 className="guest-h2">Manage your expenses and create a budget.</h2>
+      <Image
+        className="guest-img"
+        src={coverImage}
+        alt="Expense Tracker logo"
+      />
+      <h3 className="guest-h3">Welcome!</h3>
       <p className="guest-paragraph">
         Please sign in to manage your transactions.
       </p>
