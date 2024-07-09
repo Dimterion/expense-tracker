eabe2dd74a70a41844eb2e19a0f6ef2272e457f8
Dmitrii <dimterion59@gmail.com>
2024-07-09 11:38:23 +0200

    adjust transaction elements display

--- components/Balance.tsx
+++ components/Balance.tsx
@@ -5,7 +5,7 @@ const Balance = async () => {
   const { balance } = await getUserBalance();
   return (
     <>
-      <h3>Your Balance:</h3>
+      <h3 className="balance-h3">Your Balance:</h3>
       <h2 className="balance-h2">
         ${addCommas(Number(balance?.toFixed(2) ?? 0))}
       </h2>
