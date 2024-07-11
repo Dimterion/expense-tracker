0130867b7bb30e52b08d3c0423e8f7be3a38fdf6
Dmitrii <dimterion59@gmail.com>
2024-07-11 19:44:50 +0200

    change transaction delete confirmation element

--- components/TransactionItem.tsx
+++ components/TransactionItem.tsx
@@ -1,21 +1,19 @@
 "use client";
 
+import { useState } from "react";
+import { FaCheck, FaTimes } from "react-icons/fa";
 import { toast } from "react-toastify";
 import { addCommas } from "@/lib/utils";
 import { Transaction } from "@/types/transaction";
 import deleteTransaction from "@/app/actions/deleteTransaction";
 
 const TransactionItem = ({ transaction }: { transaction: Transaction }) => {
+  const [deleteConfirmation, setDeleteConfirmation] = useState(false);
+
   const className = transaction.amount < 0 ? "red" : "green";
   const sign = transaction.amount < 0 ? "-" : "+";
 
   const handleDeleteTransaction = async (transactionId: string) => {
-    const confirmed = window.confirm(
-      "Are you sure you want to delete this transaction?"
-    );
-
-    if (!confirmed) return;
-
     const { message, error } = await deleteTransaction(transactionId);
 
     if (error) {
@@ -27,19 +25,42 @@ const TransactionItem = ({ transaction }: { transaction: Transaction }) => {
 
   return (
     <li className="transactionItem-li">
-      <p className="transactionItem-paragraph">
-        <span className="transactionText">{transaction.text}</span>
-        <span className={`transactionAmount ${className}`}>
-          {sign}${addCommas(Math.abs(transaction.amount))}
-        </span>
-      </p>
-      <button
-        className="transactionItem-btn"
-        onClick={() => handleDeleteTransaction(transaction.id)}
-        aria-label="Delete transaction"
-      >
-        X
-      </button>
+      {!deleteConfirmation ? (
+        <>
+          <p className="transactionItem-paragraph">
+            <span className="transactionText">{transaction.text}</span>
+            <span className={`transactionAmount ${className}`}>
+              {sign}${addCommas(Math.abs(transaction.amount))}
+            </span>
+          </p>
+          <button
+            className="transactionItem-btn"
+            onClick={() => setDeleteConfirmation(!deleteConfirmation)}
+            aria-label="Delete transaction"
+          >
+            <FaTimes />
+          </button>
+        </>
+      ) : (
+        <>
+          <p className="transactionItem-paragraph">Delete this transaction?</p>
+          <article className="transactionItem-article">
+            <button
+              onClick={() => handleDeleteTransaction(transaction.id)}
+              className="transactionItem-btn"
+            >
+              <FaCheck />
+            </button>
+            <button
+              className="transactionItem-btn"
+              onClick={() => setDeleteConfirmation(!deleteConfirmation)}
+              aria-label="Delete transaction"
+            >
+              <FaTimes />
+            </button>
+          </article>
+        </>
+      )}
     </li>
   );
 };
