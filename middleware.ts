bc27feee2423918ac2cd564ae5fdee765f40fc20
Dmitrii <dimterion59@gmail.com>
2024-07-14 12:06:31 +0200

    update middleware.ts & colors

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,6 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware();
 
-export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+// export const config = {
+//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
