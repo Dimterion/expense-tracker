3a5f84278072758c3a45028f912dd240f515fd18
Dmitrii <dimterion59@gmail.com>
2024-07-17 11:50:38 +0200

    adjust addTransaction section

--- components/AddTransaction.tsx
+++ components/AddTransaction.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useRef, useState } from "react";
+import { FaTimes } from "react-icons/fa";
 import { toast } from "react-toastify";
 import addTransaction from "@/app/actions/addTransaction";
 
@@ -20,67 +21,59 @@ const AddTransaction = () => {
     }
   };
 
-  return (
+  return !expandSection ? (
+    <button
+      aria-label="Open Add Transaction section"
+      className="addTransaction-btn"
+      onClick={() => setExpandSection(!expandSection)}
+    >
+      Add transaction
+    </button>
+  ) : (
     <section className="addTransaction-section">
-      {!expandSection ? (
-        <button
-          aria-label="Open Add Transaction section"
-          className="addTransaction-btn"
-          onClick={() => setExpandSection(!expandSection)}
-        >
+      <button
+        aria-label="Close Add Transaction section"
+        className="addTransaction-btnClose"
+        onClick={() => setExpandSection(!expandSection)}
+      >
+        <FaTimes />
+      </button>
+      <form className="addTransaction-form" ref={formRef} action={clientAction}>
+        <article className="addTransaction-article">
+          <label className="addTransaction-label" htmlFor="text">
+            Text
+          </label>
+          <input
+            className="addTransaction-input"
+            type="text"
+            id="text"
+            name="text"
+            placeholder="Enter text"
+            maxLength={25}
+          />
+        </article>
+        <article className="addTransaction-article">
+          <label className="addTransaction-label" htmlFor="amount">
+            Amount*
+          </label>
+          <input
+            className="addTransaction-input"
+            type="number"
+            name="amount"
+            id="amount"
+            placeholder="Enter amount"
+            min={-9999999}
+            max={9999999}
+            step="0.01"
+          />
+          <span className="subText">
+            *negative - expense; positive - income
+          </span>
+        </article>
+        <button className="addTransaction-btn" aria-label="Add transaction">
           Add transaction
         </button>
-      ) : (
-        <>
-          <button
-            aria-label="Close Add Transaction section"
-            className="addTransaction-btn"
-            onClick={() => setExpandSection(!expandSection)}
-          >
-            Close window
-          </button>
-          <form
-            className="addTransaction-form"
-            ref={formRef}
-            action={clientAction}
-          >
-            <article className="addTransaction-article">
-              <label className="addTransaction-label" htmlFor="text">
-                Text
-              </label>
-              <input
-                className="addTransaction-input"
-                type="text"
-                id="text"
-                name="text"
-                placeholder="Enter text"
-                maxLength={25}
-              />
-            </article>
-            <article className="addTransaction-article">
-              <label className="addTransaction-label" htmlFor="amount">
-                Amount*
-              </label>
-              <input
-                className="addTransaction-input"
-                type="number"
-                name="amount"
-                id="amount"
-                placeholder="Enter amount"
-                min={-9999999}
-                max={9999999}
-                step="0.01"
-              />
-              <span className="subText">
-                *negative - expense; positive - income
-              </span>
-            </article>
-            <button className="addTransaction-btn" aria-label="Add transaction">
-              Add transaction
-            </button>
-          </form>
-        </>
-      )}
+      </form>
     </section>
   );
 };
