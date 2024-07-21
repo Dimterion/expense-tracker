8cd4401a2e9f275bfc2415370d6e7ad8a843d14f
Dmitrii <dimterion59@gmail.com>
2024-07-21 10:22:42 +0200

    adjust IncomeExpense comp & transactions text

--- components/IncomeExpense.tsx
+++ components/IncomeExpense.tsx
@@ -7,12 +7,16 @@ const IncomeExpense = async () => {
   return (
     <section className="incomeExpense-section">
       <article className="incomeExpense-article">
-        <h4>Income</h4>
-        <p className="green-color">${addCommas(Number(income?.toFixed(2)))}</p>
+        <h4 className="incomeExpense-h4">Income</h4>
+        <p className="incomeExpense-paragraph green-color">
+          ${addCommas(Number(income?.toFixed(2)))}
+        </p>
       </article>
       <article className="incomeExpense-article">
-        <h4>Expense</h4>
-        <p className="red-color">${addCommas(Number(expense?.toFixed(2)))}</p>
+        <h4 className="incomeExpense-h4">Expense</h4>
+        <p className="incomeExpense-paragraph red-color">
+          ${addCommas(Number(expense?.toFixed(2)))}
+        </p>
       </article>
     </section>
   );
