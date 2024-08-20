d6a12abcdf145aa5616b6f5ad99b18f141fc4b59
Dmitrii <dimterion59@gmail.com>
2024-08-20 12:02:17 +0200

    add additional info paragraph for the Guest page

--- components/Guest.tsx
+++ components/Guest.tsx
@@ -7,7 +7,7 @@ const Guest = () => {
   return (
     <section className="guest-section">
       <h1 className="guest-h1">Expense Tracker App</h1>
-      <h2 className="guest-h2">Manage your expenses and create a budget</h2>
+      <h2 className="guest-h2">Manage your expenses and create a budget*</h2>
       <Image
         className="guest-img"
         src={coverImage}
@@ -22,6 +22,10 @@ const Guest = () => {
           Sign In
         </button>
       </SignInButton>
+      <aside className="guest-aside">
+        *Please note that the app is in Work In Progress state which means that
+        it may change in the future and any saved data may be deleted from it.
+      </aside>
     </section>
   );
 };
