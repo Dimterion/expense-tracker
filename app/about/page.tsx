7c016a945fd99f2d255156070e6cd65421fca90a
Dmitrii <dimterion59@gmail.com>
2024-11-13 12:40:21 +0100

    adjust About page content & style

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -17,6 +17,19 @@ const About = async () => {
         Expense Tracker is an application designed to help track users income
         and expenses.
       </p>
+      <h3>Features</h3>
+      <ul className="aboutPage-ul">
+        <li>
+          Create account (standard login/password or Google authentication)
+        </li>
+        <li>Add transactions (expenses/income)</li>
+        <li>Delete transactions (expenses/income)</li>
+        <li>Track transactions history</li>
+        <li>
+          Track overall balance as well as current sum of expenses and income
+        </li>
+        <li>Manage account details</li>
+      </ul>
     </main>
   );
 };
