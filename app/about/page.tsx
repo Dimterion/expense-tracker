e4ca11e06af868c460103d59488493c38cb2138b
Dmitrii <dimterion59@gmail.com>
2024-11-13 12:33:35 +0100

    adjust About page content & elements

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,22 @@
+import Image from "next/image";
+import coverImage from "@/assets/images/expense_tracker_readme_header.svg";
+
 const About = async () => {
   return (
     <main className="aboutPage-main">
       <h2 className="aboutPage-h2">Expense Tracker App</h2>
+      <Image
+        className="aboutPage-img"
+        src={coverImage}
+        alt="Expense Tracker header image"
+        width={1000}
+        height={400}
+        priority
+      />
+      <p className="aboutPage-paragraph">
+        Expense Tracker is an application designed to help track users income
+        and expenses.
+      </p>
     </main>
   );
 };
