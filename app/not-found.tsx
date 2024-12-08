5766546eea3c68e886fed9b68339a6b8b0d7617b
Dmitrii <dimterion59@gmail.com>
2024-12-08 12:11:19 +0100

    add NotFound page styling

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,11 +2,15 @@ import Link from "next/link";
 
 const NotFound = () => {
   return (
-    <main>
-      <p>404</p>
-      <h2>Page not found</h2>
-      <p>Please check the link and try again.</p>
-      <Link href="/">Home page</Link>
+    <main className="notFoundPage-main">
+      <p className="notFoundPage-paragraph">404</p>
+      <h2 className="notFoundPage-h2">Page not found</h2>
+      <p className="notFoundPage-paragraph">
+        Please check the link and try again.
+      </p>
+      <Link href="/" className="notFoundPage-link">
+        Home page
+      </Link>
     </main>
   );
 };
